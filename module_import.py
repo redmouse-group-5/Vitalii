0c2794cede2aa028a9e2114ed84b19c2866434a8
pehas <pehas.90@gmail.com>
2016-12-05 20:49:58 +0600

    Added package with modules

---
+++ module_import.py
@@ -0,0 +1,4 @@
+from math import add as madd
+import my_mod
+
+print(my_mod.add(1,3))
---
+++ module_import2.py
@@ -0,0 +1,5 @@
+from  my_pack import my_mod
+from my_pack import my_add
+
+print(my_add.add(1,3))
+print(my_mod.division(1,3))
