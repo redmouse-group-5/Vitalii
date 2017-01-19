4df72f7afaef62190c822ddfd027b5de3d3054dc
pehas <pehas.90@gmail.com>
2017-01-19 19:54:35 +0600

    Update middleware

--- first/article/middleware/middleware.py
+++ first/article/middleware/middleware.py
@@ -2,6 +2,6 @@ from django.utils.deprecation import MiddlewareMixin
 
 class SimpleMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        if 'city' not in request:
+        if not hasattr(request, 'city'):
             request.city = "Bishkek"
         return None
