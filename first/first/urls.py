62083bcf274124f7bb4d68ce83ec8f4e562425f1
pehas <pehas.90@gmail.com>
2017-01-23 19:19:47 +0600

    Return def

--- first/first/urls.py
+++ first/first/urls.py
@@ -19,7 +19,7 @@ from article.views import current_datetime, get_index, google
 from django.conf import settings
 from django.conf.urls.static import static
 
-from comment.views import CommentView
+from comment.views import comment_add
 from page.views import get_page
 
 urlpatterns = [
@@ -29,7 +29,7 @@ urlpatterns = [
     url(r'^current_datatime/$', current_datetime),
     url(r'^articles/', include('article.urls', namespace="article")),
     url(r'^artic/', include('article.urls', namespace="artic")),
-    url(r'^comments_add/(?P<id>\d+)/$', CommentView.as_view(), name="comment_add"),
+    url(r'^comments_add/(?P<id>\d+)/$', comment_add, name="comment_add"),
     url(r'^page/(?P<slug>\S+)/$', get_page, name="page")
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
